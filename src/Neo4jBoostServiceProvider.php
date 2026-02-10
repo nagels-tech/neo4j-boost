01d181cb46da1e0984c6179096d85273ac7976b0
cod3xa <zaeem@nagels.tech>
2026-02-10 12:08:38 +0530

    feat: moved config and routes folder in root directory to follow laravel package builder standard

--- src/Neo4jBoostServiceProvider.php
+++ src/Neo4jBoostServiceProvider.php
@@ -2,21 +2,22 @@
 
 namespace NagelsTech\Neo4jBoost;
 
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
 
 class Neo4jBoostServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/neo4j-boost.php', 'neo4j-boost');
+        $this->mergeConfigFrom(__DIR__ . '/../config/neo4j-boost.php', 'neo4j-boost');
     }
 
     public function boot(): void
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/mcp.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/mcp.php');
 
         $this->publishes([
-            __DIR__ . '/config/neo4j-boost.php' => config_path('neo4j-boost.php'),
+            __DIR__ . '/../config/neo4j-boost.php' => config_path('neo4j-boost.php'),
         ], 'neo4j-boost-config');
     }
 }
